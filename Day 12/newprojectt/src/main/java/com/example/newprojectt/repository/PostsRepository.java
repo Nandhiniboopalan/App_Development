90711c19710de914f8b5ad548059e0db31356f34
Nandhiniboopalan <118872477+Nandhiniboopalan@users.noreply.github.com>
2023-10-06 15:28:08 +0530

    Delete Day 12/newprojectt directory

--- Day 12/newprojectt/src/main/java/com/example/newprojectt/repository/PostsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.newprojectt.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.newprojectt.model.PostsEntity;
-
-public interface PostsRepository extends JpaRepository<PostsEntity, Long>{
-
-}
